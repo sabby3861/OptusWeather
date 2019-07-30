20a7be7ee8953ea8c3001a2b36ead0a664a9f977
srinu <srinu.lagudu@gmail.com>
2019-07-30 18:19:34 +0530

    [Optus]: Implemented Whether API and Codable class to save the data in core data

---
+++ OptusWeather/OPWeather.swift
@@ -0,0 +1,61 @@
+//
+//  OPWeather.swift
+//  OptusWeather
+//
+//  Created by techm on 30/07/19.
+//  Copyright © 2019 techm. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+ class OPWeather: NSManagedObject, Codable {
+    /*public let id: Int
+    public let visibility: Int?
+    public let name: String?*/
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "id"
+        case visibility = "visibility"
+        case name = "name"
+    }
+    
+    // MARK: - Core Data Managed Object
+    @NSManaged var id: Int32
+    @NSManaged var visibility: Int32
+    @NSManaged var name: String?
+  
+    // MARK: - Decodable
+    required convenience init(from decoder: Decoder) throws {
+        guard let codingUserInfoKeyManagedObjectContext = CodingUserInfoKey.managedObjectContext,
+            let managedObjectContext = decoder.userInfo[codingUserInfoKeyManagedObjectContext] as? NSManagedObjectContext,
+            let entity = NSEntityDescription.entity(forEntityName: GJCoreData.name.rawValue, in: managedObjectContext) else {
+                fatalError("Failed to decode Contacts")
+        }
+        
+        self.init(entity: entity, insertInto: managedObjectContext)
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = (try container.decodeIfPresent(Int32.self, forKey: .id))!
+        self.visibility = (try container.decodeIfPresent(Int32.self, forKey: .visibility))!
+        self.name = try container.decodeIfPresent(String.self, forKey: .name)
+
+    }
+    
+    // MARK: - Encodable
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(visibility, forKey: .visibility)
+        try container.encode(name, forKey: .name)
+    }
+}
+
+public extension CodingUserInfoKey {
+    // Helper property to retrieve the Core Data managed object context
+    static let managedObjectContext = CodingUserInfoKey(rawValue: "managedObjectContext")
+}
+
+enum GJCoreData: String {
+    case name = "OPWeatherInfo"
+}
