17cde65d047338f330cf7ace737a8b758e7fc2f6
srinu <srinu.lagudu@gmail.com>
2019-08-01 01:39:30 +0530

    [Optus]: Added Generic class for storyboard

---
+++ OptusWeather/Helper/OPStoryBoard.swift
@@ -0,0 +1,47 @@
+//
+//  OPStoryBoard.swift
+//  OptusWeather
+//
+//  Created by techm on 01/08/19.
+//  Copyright © 2019 techm. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIStoryboard {
+    enum Storyboard: String {
+        case Main
+        var filename: String {
+            return rawValue.capitalized
+        }
+    }
+    convenience init(storyboard: Storyboard, bundle: Bundle? = nil) {
+        self.init(name: storyboard.filename, bundle: bundle)
+    }
+    class func storyboard(storyboard: Storyboard, bundle: Bundle? = nil) -> UIStoryboard {
+        return UIStoryboard(name: storyboard.filename, bundle: bundle)
+    }
+    
+    func instantiateVieController<T>() -> T where T: StoryboardIdentifiable {
+        
+        guard let viewController = self.instantiateViewController(withIdentifier: T.storyboardIdentifier) as? T else {
+            fatalError("Couldn't instantiate view controller with identifier \(T.storyboardIdentifier) ")
+        }
+        return viewController
+    }
+    
+}
+
+
+protocol StoryboardIdentifiable {
+    static var storyboardIdentifier: String { get }
+}
+
+extension StoryboardIdentifiable where Self: UIViewController {
+    static var storyboardIdentifier: String {
+        return String(describing: self)
+    }
+}
+
+extension UIViewController: StoryboardIdentifiable{}
